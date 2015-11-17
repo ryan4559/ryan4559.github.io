edad717700a54a37990d5612e479c6773f8c28b2
Ryan Weng <ryan4559@gmail.com>
2015-11-17 21:18:23 +0800

    fixed light

--- CG/cube2_files/cube2.js
+++ CG/cube2_files/cube2.js
@@ -205,7 +205,7 @@ function render() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
-    if (! paused) theta[axis] += 2.0;
+    if (! paused) theta[axis] += 1.0;
 	if (depthTest) gl.enable(gl.DEPTH_TEST); else gl.disable(gl.DEPTH_TEST);
 
     gl.uniformMatrix4fv( modelingLoc,   0, flatten(modeling) );
