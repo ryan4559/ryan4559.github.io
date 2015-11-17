79453d6883dfda165be4ced6c1365c3c9d24e496
Ryan Weng <ryan4559@gmail.com>
2015-11-17 17:51:10 +0800

    add cube2

---
+++ CG/cube2_files/cube2.js
@@ -0,0 +1,218 @@
+
+var gl;
+var points;
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [ 0, 0, 0 ];
+var paused = 0;
+var depthTest = 1;
+
+// ModelView and Projection matrices
+var modelingLoc, viewingLoc, projectionLoc;
+var modeling, viewing, projection;
+
+var numVertices  = 36;
+
+var pointsArray = [];
+var colorsArray = [];
+var normalsArray = [];
+
+var vertices = [
+	vec4( -0.5, -0.5,  0.5, 1 ),
+	vec4( -0.5,  0.5,  0.5, 1 ),
+	vec4(  0.5,  0.5,  0.5, 1 ),
+	vec4(  0.5, -0.5,  0.5, 1 ),
+	vec4( -0.5, -0.5, -0.5, 1 ),
+	vec4( -0.5,  0.5, -0.5, 1 ),
+	vec4(  0.5,  0.5, -0.5, 1 ),
+	vec4(  0.5, -0.5, -0.5, 1 )
+];
+
+// Using off-white cube for testing
+var vertexColors = [
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 ),  
+	vec4( 1.0, 1.0, 0.8, 1.0 )
+];
+
+// var vertexColors = [
+	// vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+	// vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+	// vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+	// vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+	// vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+	// vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+	// vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+	// vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+// ];
+
+var lightPosition = vec4( 0.0, 1.0, -2.0, 1.0 );
+
+var materialAmbient = vec4( 0.25, 0.25, 0.25, 1.0 );
+var materialDiffuse = vec4( 0.8, 0.8, 0.8, 1.0);
+var materialSpecular = vec4( 10.0, 1.0, 1.0, 1.0 );
+var materialShininess = 10.0;
+
+function quad(a, b, c, d) {
+
+     var t1 = subtract(vertices[b], vertices[a]);
+     var t2 = subtract(vertices[c], vertices[b]);
+     var normal = cross(t1, t2);  // cross returns vec3
+     var normal = vec4(normal);
+     normal = normalize(normal);
+
+     pointsArray.push(vertices[a]); 
+	 colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[b]); 
+	 colorsArray.push(vertexColors[b]);
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[c]); 
+	 colorsArray.push(vertexColors[c]);
+     normalsArray.push(normal);   
+
+     pointsArray.push(vertices[a]);  
+	 colorsArray.push(vertexColors[a]);
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[c]); 
+	 colorsArray.push(vertexColors[c]);
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[d]); 
+	 colorsArray.push(vertexColors[d]);
+     normalsArray.push(normal);    
+}
+
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+	// Generate pointsArray[], colorsArray[] and normalsArray[] from vertices[] and vertexColors[].
+	// We don't use indices and ELEMENT_ARRAY_BUFFER (as in previous example)
+	// because we need to assign different face normals to shared vertices.
+	colorCube();
+    
+    // vertex array attribute buffer
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    // color array atrribute buffer
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    // normal array atrribute buffer
+
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+    
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
+
+
+	// uniform variables in shaders
+    modelingLoc   = gl.getUniformLocation(program, "modelingMatrix"); 
+    viewingLoc    = gl.getUniformLocation(program, "viewingMatrix"); 
+    projectionLoc = gl.getUniformLocation(program, "projectionMatrix"); 
+
+    gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), 
+       flatten(lightPosition) );
+    gl.uniform4fv( gl.getUniformLocation(program, "materialAmbient"),
+       flatten(materialAmbient));
+    gl.uniform4fv( gl.getUniformLocation(program, "materialDiffuse"),
+       flatten(materialDiffuse) );
+    gl.uniform4fv( gl.getUniformLocation(program, "materialSpecular"), 
+       flatten(materialSpecular) );	       
+    gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess);
+
+    //event listeners for buttons 
+    document.getElementById( "xButton" ).onclick = rotateX;
+    document.getElementById( "yButton" ).onclick = rotateY;
+    document.getElementById( "zButton" ).onclick = rotateZ;
+    document.getElementById( "pButton" ).onclick = function() {paused=!paused;};
+    document.getElementById( "dButton" ).onclick = function() {depthTest=!depthTest;};
+	
+    render();
+};
+
+function rotateX() {
+	paused = 0;
+    axis = xAxis;
+};
+function rotateY() {
+	paused = 0;
+	axis = yAxis;
+};
+function rotateZ() {
+	paused = 0;
+	axis = zAxis;
+};
+
+
+function render() {
+	modeling = mult(rotate(theta[xAxis], 1, 0, 0),
+	                mult(rotate(theta[yAxis], 0, 1, 0),rotate(theta[zAxis], 0, 0, 1)));
+
+	viewing = lookAt([0,0,-2], [0,0,0], [0,1,0]);
+
+	projection = perspective(45, 1.0, 1.0, 3.0);
+
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+    if (! paused) theta[axis] += 2.0;
+	if (depthTest) gl.enable(gl.DEPTH_TEST); else gl.disable(gl.DEPTH_TEST);
+
+    gl.uniformMatrix4fv( modelingLoc,   0, flatten(modeling) );
+    gl.uniformMatrix4fv( viewingLoc,    0, flatten(viewing) );
+	gl.uniformMatrix4fv( projectionLoc, 0, flatten(projection) );
+
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+
+    requestAnimFrame( render );
+}
